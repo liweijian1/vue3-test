0bbb238b26de95a3b4262fe85363b788efdf2d06
lwj <928719621@qq.com>
2020-12-08 15:02:31 +0800

    vue3

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{count}}</h2>
   </div>
 </template>
+<script>
+import {state} from './../observable/store'
+export default {
+   data(){},
+   computed:{
+     count(){
+       return state.count
+     }
+   }
+}
+</script>
